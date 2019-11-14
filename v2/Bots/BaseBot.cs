734454cdb65b4bf026bb828dbcec6f05b7b3dd70
Marc Bryan <mbryan@invidi.com>
2019-11-14 11:30:50 -0700

    Add FortStealerBot and helper methods

--- v2/Bots/BaseBot.cs
+++ v2/Bots/BaseBot.cs
@@ -39,5 +39,68 @@ namespace Cell.Bots
 		{
 			return guys.Where(g => g.Type == type).ToList();
 		}
+
+		public static List<Guy> FilterAverage(List<Guy> guys)
+		{
+			return guys.Where(g => g.Type == GuyType.AVERAGE).ToList();
+		}
+
+		public static List<Guy> FilterArmored(List<Guy> guys)
+		{
+			return guys.Where(g => g.Type == GuyType.ARMORED).ToList();
+		}
+
+		public static List<Guy> FilterBeefy(List<Guy> guys)
+		{
+			return guys.Where(g => g.Type == GuyType.BEEFY).ToList();
+		}
+
+		public static List<Guy> FilterRunner(List<Guy> guys)
+		{
+			return guys.Where(g => g.Type == GuyType.RUNNER).ToList();
+		}
+
+		public static double GetDistanceBetween(Point source, Point dest)
+		{
+			double dX = source.X - dest.X;
+			double dY = source.Y - dest.Y;
+			return Math.Sqrt(dX * dX + dY * dY);
+		}
+
+		public static Move Move(Fort srcFort, Fort destFort, int numGuys, GuyType type)
+		{
+			return new Move { SourceFortID = srcFort.ID, DestinationFortID = destFort.ID, NumGuys = numGuys, GuyType = type };
+		}
+
+		public static List<Move> MoveAll(Fort srcFort, Fort destFort)
+		{
+			return new List<Move>
+			{
+				MoveAverage(srcFort, destFort, FilterAverage(srcFort.DefendingGuys).Count),
+				MoveArmored(srcFort, destFort, FilterArmored(srcFort.DefendingGuys).Count),
+				MoveBeefy(srcFort, destFort, FilterBeefy(srcFort.DefendingGuys).Count),
+				MoveRunner(srcFort, destFort, FilterRunner(srcFort.DefendingGuys).Count)
+			};
+		}
+
+		public static Move MoveAverage(Fort srcFort, Fort destFort, int numGuys)
+		{
+			return new Move { SourceFortID = srcFort.ID, DestinationFortID = destFort.ID, NumGuys = numGuys, GuyType = GuyType.AVERAGE };
+		}
+
+		public static Move MoveArmored(Fort srcFort, Fort destFort, int numGuys)
+		{
+			return new Move { SourceFortID = srcFort.ID, DestinationFortID = destFort.ID, NumGuys = numGuys, GuyType = GuyType.ARMORED };
+		}
+
+		public static Move MoveBeefy(Fort srcFort, Fort destFort, int numGuys)
+		{
+			return new Move { SourceFortID = srcFort.ID, DestinationFortID = destFort.ID, NumGuys = numGuys, GuyType = GuyType.BEEFY };
+		}
+
+		public static Move MoveRunner(Fort srcFort, Fort destFort, int numGuys)
+		{
+			return new Move { SourceFortID = srcFort.ID, DestinationFortID = destFort.ID, NumGuys = numGuys, GuyType = GuyType.RUNNER };
+		}
 	}
 }
---
+++ v2/Bots/FortStealerBot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Cell.Bots
+{
+	// Waits in their fort until a fort is empty then steals it as quickly as it can
+	class FortStealerBot : BaseBot
+	{
+		public override List<Guy> SetStartingArmy()
+		{
+			var army = new List<Guy>();
+			for (var i = 0; i < 10; i++)
+			{
+				army.Add(GuyFactory.CreateGuy(GuyType.RUNNER));
+			}
+			return army;
+		}
+
+		public override List<Move> Do(string boardString)
+		{
+			var board = JsonConvert.DeserializeObject<BoardState>(boardString);
+
+			var friendlyForts = GetFriendlyForts(m_player, board);
+			var enemyForts = GetEnemyForts(m_player, board);
+			var neutralForts = GetNeutralForts(board);
+
+			var moves = new List<Move>();
+
+			if (friendlyForts.Count > 0)
+			{
+				var distances = new Dictionary<Fort, Fort>();
+
+				// find the closest friendly fort to the closest empty forts
+				// empty means no owner or an owner but no guys and no birthspeeds
+				var forts = neutralForts.Concat(enemyForts.Where(f => f.DefendingGuys.Count() < 1 && f.BirthSpeed < 1).ToList());
+				foreach (var targetFort in forts)
+				{
+					double shortestDistance = double.MaxValue;
+					foreach (var fort in friendlyForts)
+					{
+						var distance = GetDistanceBetween(targetFort.Location, fort.Location);
+						if (distance < shortestDistance)
+						{
+							shortestDistance = distance;
+							distances[targetFort] = fort;
+						}
+					}
+				}
+
+				foreach (KeyValuePair<Fort, Fort> entry in distances)
+				{
+					var destFort = entry.Key;
+					var srcFort = entry.Value;
+
+					moves.Concat(MoveAll(srcFort, destFort));
+				}
+			}
+
+			return moves;
+		}
+	}
+}
